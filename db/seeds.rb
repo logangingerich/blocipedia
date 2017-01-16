9b6c768f2a566b29e3f620359e17f3f7b0aa2eb5
Logan Gingerich <logangingerich@yahoo.com>
2017-01-16 07:59:46 -0700

    Faker gem used to seed fake wiki and user data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Creates Wikis
+50.times do
+  Wiki.create!(
+    title: Faker::StarWars.planet,
+    body: Faker::StarWars.quote
+  )
+end
+wikis = Wiki.all
+
+# Creates Users
+10.times do
+  User.create!(
+    email: Faker::Internet.email,
+    password: Faker::Internet.password
+  )
+end
+users = User.all
+
+puts "Seed finished"
+puts "#{User.count} users created!"
+puts "#{Wiki.count} wikis created!"
