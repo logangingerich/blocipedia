727316be38ac08a560cbf2e6bb029d5bac0aef33
Logan Gingerich <logangingerich@yahoo.com>
2017-01-19 08:27:18 -0700

    Used Redcarpet to properly render Markdown syntax in wikis.

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -37,7 +37,10 @@ class WikisController < ApplicationController
     @wiki = Wiki.find(params[:id])
     @wiki.title = params[:wiki][:title]
     @wiki.body = params[:wiki][:body]
-    @wiki.private = params[:wiki][:private]
+    
+    if current_user.admin? || current_user.premium?
+      @wiki.private = params[:wiki][:private]
+    end
 
     authorize @wiki
     if @wiki.save
